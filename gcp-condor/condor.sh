a6ce096bdb47cd0e7ca0aa5b4233cf3dd5b3c483
YoungsooJang <jys5609@gmail.com>
2026-05-25 17:00:30 +0000

    .

--- gcp-condor/condor.sh
+++ gcp-condor/condor.sh
@@ -6,6 +6,6 @@ export HOME=/home/jys5609
 conda activate cem
 cd /mnt/nfs/CEM-RoPose
 
-$HOME/anaconda3/envs/cem/bin/python3 -m Simulation.Cem.dream_cem --num-workers 60 --input-root /mnt/nfs/DreamDataset/Azure --pid=$1
+$HOME/anaconda3/envs/cem/bin/python3 -m Simulation.Cem.dream_cem --num-workers 60 --input-root /mnt/nfs/DreamDataset --pid=$1
 # $HOME/anaconda3/envs/cem/bin/python3 -m Simulation.Cem.dream_cem --num-workers 60 --input-root /mnt/nfs/DreamDataset/Kinect --pid=$1
 # $HOME/anaconda3/envs/cem/bin/python3 -m Simulation.Cem.dream_cem --num-workers 60 --input-root /mnt/nfs/DreamDataset/Realsense --pid=$1
